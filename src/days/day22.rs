35534f1597a1e87d5d4566e199d988fd5631eb1d
Maitgon <aitor98gfgb@gmail.com>
2024-12-24 13:46:44 +0100

    refactor

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -58,43 +58,6 @@ fn both(input: &Vec<u64>) -> (u64, u64) {
     (sol1_result, *seqs.values().max().unwrap())
 }
 
-/*
-fn both(input: &Vec<u64>) -> (u64, u64) {
-    let mut sol1 = 0;
-    let mut secret_numbers: Vec<Vec<u64>> = Vec::new();
-    for n in input {
-        let mut n = *n;
-        let mut secrets: Vec<u64> = vec![n % 10];
-
-        for i in 0..2000 {
-            n = next_secret_number(n);
-            if i == 1999 {
-                sol1 += n;
-            }
-            secrets.push(n % 10);
-        }
-
-        secret_numbers.push(secrets);
-    }
-
-    let mut seqs: FxHashMap<(u64, u64, u64, u64), u64> = FxHashMap::default();
-
-    for vals in secret_numbers {
-        let mut seqs_used: FxHashSet<(u64, u64, u64, u64)> = FxHashSet::default();
-        for i in 0..vals.len() - 4 {
-            if let [a, b, c, d, e] = vals[i..i+5] {
-                let seq = (b-a, c-b, d-c, e-d);
-                if !seqs_used.insert(seq) {
-                    continue;
-                }
-                *seqs.entry(seq).or_insert(0) += e;
-            }
-        }
-    }
-
-    (sol1, *seqs.values().max().unwrap())
-}
-*/
 fn next_secret_number(mut n: u64) -> u64 {
     n = (n ^ (n << 6)) & 0xFFFFFF;
     n = (n ^ (n >> 5)) & 0xFFFFFF;
